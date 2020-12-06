4e68192f5a322c25c1adb2672fb775be5b38d35f
Inseok Lee <dlunch@gmail.com>
2020-12-06 11:18:00 +0900

    kbcard login

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,55 @@
+use std::env;
+use std::time::Duration;
+
+use async_std::task;
+use dotenv::dotenv;
 use fantoccini::{Client, Locator};
 use serde_json::json;
 use webdriver::capabilities::Capabilities;
 
-// let's set up the sequence of steps we want the browser to take
 #[async_std::main]
 async fn main() -> Result<(), fantoccini::error::CmdError> {
-    let mut cap = Capabilities::new();
-    let arg = json!({"args": ["-headless"]});
-    cap.insert("moz:firefoxOptions".to_string(), arg);
+    dotenv().ok();
 
-    let mut c = Client::with_capabilities("http://localhost:4444", cap)
+    let kbcard_id = env::var("KBCARD_ID").unwrap();
+    let kbcard_password = env::var("KBCARD_PW").unwrap();
+    let webdriver_host = env::var("WEBDRIVER_HOST").unwrap();
+
+    let webdriver_headless = env::var("WEBDRIVER_HEADLESS").is_ok();
+
+    let mut cap = Capabilities::new();
+    if webdriver_headless {
+        let arg = json!({"args": ["-headless"]});
+        cap.insert("moz:firefoxOptions".to_string(), arg);
+    }
+
+    let mut c = Client::with_capabilities(&webdriver_host, cap)
         .await
         .expect("failed to connect to WebDriver");
 
-    // first, go to the Wikipedia page for Foobar
-    c.goto("https://en.wikipedia.org/wiki/Foobar").await?;
-    let url = c.current_url().await?;
-    assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foobar");
+    c.goto("https://m.kbcard.com").await?;
 
-    // click "Foo (disambiguation)"
-    c.find(Locator::Css(".mw-disambig")).await?.click().await?;
+    // To login page
+    c.find(Locator::Css(".hmBtn")).await?.click().await?;
+    c.find(Locator::Css(".btnLogin")).await?.click().await?;
 
-    // click "Foo Lake"
-    c.find(Locator::LinkText("Foo Lake")).await?.click().await?;
+    // Show login form
+    c.find(Locator::LinkText("아이디")).await?.click().await?;
 
-    let url = c.current_url().await?;
-    println!("{}", url.as_ref());
+    // Input credentials
+    c.find(Locator::Id("userId")).await?.send_keys(&kbcard_id).await?;
+    c.find(Locator::Id("userPwd")).await?.click().await?;
+    for password_char in kbcard_password.chars() {
+        c.find(Locator::XPath(&format!("//*[@aria-label=\"{}\"]", password_char)))
+            .await?
+            .click()
+            .await?;
+    }
+    c.find(Locator::XPath("//*[@aria-label=\"입력완료\"]")).await?.click().await?;
+
+    c.find(Locator::Id("btnIdPwdLogin")).await?.click().await?;
+
+    task::sleep(Duration::from_secs(10)).await;
 
     c.close().await
 }
