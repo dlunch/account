f9425f60bd88b50cdb7a36236ae5ce4a7e7f81cf
Inseok Lee <dlunch@gmail.com>
2021-02-04 21:42:35 +0900

    GRpc server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,30 @@
-use std::boxed::Box;
-use std::env;
-use std::error::Error;
+use async_trait::async_trait;
+use tonic::{transport::Server, Request, Response, Status};
 
-use dotenv::dotenv;
+mod pb {
+    tonic::include_proto!("auth");
+}
+
+use pb::{LoginRequest, LoginResponse};
+
+struct Auth {}
+
+#[async_trait]
+impl pb::auth_server::Auth for Auth {
+    async fn login(&self, request: Request<LoginRequest>) -> Result<Response<LoginResponse>, Status> {
+        let username = request.into_inner().username;
+
+        Ok(Response::new(LoginResponse { token: username }))
+    }
+}
 
 #[async_std::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv().ok();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:9090".parse().unwrap();
+    let auth = Auth {};
 
-    let kbcard_id = env::var("KBCARD_ID").unwrap();
-    let kbcard_password = env::var("KBCARD_PW").unwrap();
-
-    scrap::scrap_kbcard(&kbcard_id, &kbcard_password).await?;
+    let service = pb::auth_server::AuthServer::new(auth);
+    Server::builder().add_service(service).serve(addr).await?;
 
     Ok(())
 }
