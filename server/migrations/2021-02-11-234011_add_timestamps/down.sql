93cd7388bca3bee6fa7428998df6bb04a1db89f3
Inseok Lee <dlunch@gmail.com>
2021-02-12 14:36:51 +0900

    Add updated_at default value

--- server/migrations/2021-02-11-234011_add_timestamps/down.sql
+++ server/migrations/2021-02-11-234011_add_timestamps/down.sql
@@ -1,5 +1,9 @@
 ALTER TABLE "users" DROP COLUMN created_at;
 ALTER TABLE "users" DROP COLUMN updated_at;
 
+DROP TRIGGER set_updated_at ON "users";
+
 ALTER TABLE "cards" DROP COLUMN created_at;
 ALTER TABLE "cards" DROP COLUMN updated_at;
+
+DROP TRIGGER set_updated_at ON "cards";
