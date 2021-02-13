80ac687321b98653df4b02d5618922d28cd11c43
Inseok Lee <dlunch@gmail.com>
2021-02-13 12:42:18 +0900

    Fix local development

--- client/wasm/src/lib.rs
+++ client/wasm/src/lib.rs
@@ -20,6 +20,8 @@ pub fn start(grpc_host: &str) {
     #[cfg(debug_assertions)]
     console_log::init_with_level(log::Level::Trace).unwrap();
 
+    log::debug!("GRPC_HOST: {}", grpc_host);
+
     context::Context::init(grpc_host);
 
     yew::start_app::<app::App>();
