466fe09a9924ee635d93e593f3bbebc88bc6cdb0
Inseok Lee <dlunch@gmail.com>
2021-02-14 13:46:12 +0900

    typo

--- client/wasm/src/grpc.rs
+++ client/wasm/src/grpc.rs
@@ -1,5 +1,5 @@
 use typescript_wasm_bindgen::typescript_wasm_bindgen;
 use wasm_bindgen::prelude::{wasm_bindgen, JsValue};
 
-typescript_wasm_bindgen!("client/src/proto/auth_grpc_web_pb.d.ts", "proto/auth_grpcweb_pb");
+typescript_wasm_bindgen!("client/src/proto/auth_grpc_web_pb.d.ts", "proto/auth_grpc_web_pb");
 typescript_wasm_bindgen!("client/src/proto/auth_pb.d.ts", "proto/auth_pb");
