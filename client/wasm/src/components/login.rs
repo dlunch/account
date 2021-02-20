dc6c805706f606bda70787cee2a7a541142c9bc9
Inseok Lee <dlunch@gmail.com>
2021-02-20 12:18:51 +0900

    Implement login

--- client/wasm/src/components/login.rs
+++ client/wasm/src/components/login.rs
@@ -1,19 +1,56 @@
-use yew::prelude::{html, Component, ComponentLink, Html, ShouldRender};
+use wasm_bindgen::JsValue;
+use wasm_bindgen_futures::spawn_local;
+use web_sys::HtmlInputElement;
+use yew::prelude::{html, Component, ComponentLink, Html, NodeRef, ShouldRender};
 
-pub struct Login {}
+use crate::context::Context;
+use crate::grpc::{AuthPromiseClient, LoginRequest};
+use crate::router::{change_route, AppRoute};
 
-pub enum Msg {}
+pub struct Login {
+    link: ComponentLink<Self>,
+    username: NodeRef,
+    password: NodeRef,
+}
+
+pub enum Msg {
+    Submit,
+}
 
 impl Component for Login {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Self {}
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            username: NodeRef::default(),
+            password: NodeRef::default(),
+        }
     }
 
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
-        true
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Submit => {
+                let username = self.username.cast::<HtmlInputElement>().unwrap().value();
+                let password = self.password.cast::<HtmlInputElement>().unwrap().value();
+
+                let login_request = LoginRequest::new();
+                login_request.setUsername(&username);
+                login_request.setPassword(&password);
+
+                let context = Context::get();
+                let grpc_host = context.grpc_host.clone();
+                spawn_local(async move {
+                    let auth_client = AuthPromiseClient::new(&grpc_host, JsValue::NULL, JsValue::NULL);
+                    auth_client.login(JsValue::from(login_request), JsValue::NULL).await;
+
+                    change_route(AppRoute::Index);
+                });
+            }
+        }
+
+        false
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -23,7 +60,11 @@ impl Component for Login {
     fn view(&self) -> Html {
         html! {
             <div class="login">
-                <p> { "login" } </p>
+                <form>
+                    <p> <label for="username">{ "Username :" }</label> <input id="username" type="text" ref=self.username.clone() /> </p>
+                    <p> <label for="password">{ "Password :" }</label> <input id="password" type="password" ref=self.password.clone() /> </p>
+                    <p> <button onclick=self.link.callback(|_| Msg::Submit) type="button"> { "Submit" }</button> </p>
+                </form>
             </div>
         }
     }
