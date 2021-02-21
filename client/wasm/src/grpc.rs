de5ddf8a0db28ffd454f4284a964417f3977fa6c
Inseok Lee <dlunch@gmail.com>
2021-02-21 16:36:23 +0900

    Add card grpc files

--- client/wasm/src/grpc.rs
+++ client/wasm/src/grpc.rs
@@ -3,3 +3,6 @@ use wasm_bindgen::prelude::{wasm_bindgen, JsValue};
 
 typescript_wasm_bindgen!("client/src/proto/auth_grpc_web_pb.d.ts", "proto/auth_grpc_web_pb");
 typescript_wasm_bindgen!("client/src/proto/auth_pb.d.ts", "proto/auth_pb");
+
+typescript_wasm_bindgen!("client/src/proto/card_grpc_web_pb.d.ts", "proto/card_grpc_web_pb");
+typescript_wasm_bindgen!("client/src/proto/card_pb.d.ts", "proto/card_pb");
