1c02f478f7b02312a9a46a4d4c399fc4d6ab68ef
Inseok Lee <git@dlun.ch>
2021-05-09 16:34:42 +0900

    Adjust prost build option

--- scrap/build.rs
+++ scrap/build.rs
@@ -1,6 +1,5 @@
 fn main() {
     prost_build::Config::new()
-        .protoc_arg("--experimental_allow_proto3_optional")
-        .compile_protos(&["internal/internal.proto", "common.proto"], &["../proto"])
+        .compile_protos(&["../proto/internal/internal.proto", "../proto/common.proto"], &["../proto"])
         .unwrap()
 }
